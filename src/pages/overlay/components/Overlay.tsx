d946fa94386463971402facbed598ededcd24a40
abdullahmorrison <abdullahmorrison@gmail.com>
2023-02-25 09:38:10 -0500

    set up overlay components

---
+++ src/pages/overlay/components/Overlay.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './overlay.module.css'
+
+export default function Overlay(){
+  return (
+    <div className={styles.overlay}>
+      <h1>Streamer Bingo</h1>
+    </div>
+  )
+}
