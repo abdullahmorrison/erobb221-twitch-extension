e7d4a12ee61db180a13121e1169bd13599d1beee
abdullahmorrison <abdullahmorrison@gmail.com>
2023-02-25 09:38:27 -0500

    set up panel components

--- src/pages/panel/index.tsx
+++ src/pages/panel/index.tsx
@@ -5,6 +5,6 @@ const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 
 root.render(
   <React.StrictMode>
-    <div>panel</div>
+    <h1>panel</h1>
   </React.StrictMode>
 );
