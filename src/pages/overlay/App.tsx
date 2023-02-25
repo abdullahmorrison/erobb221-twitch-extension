85b10a7d3687c4d1b476e5c2bb7c6e359f1e2ac8
abdullahmorrison <abdullahmorrison@gmail.com>
2023-02-25 16:05:37 -0500

    overlay app (body) styles

--- src/pages/overlay/App.tsx
+++ src/pages/overlay/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Overlay from './components/overlay'
 
+import styles from './app.module.css'
+
 export default function App(){
   return (
-    <div>
+    <div className={styles.app}>
       <Overlay />
     </div>
   )
