a2f4be479aa34e0b01208ab73de7e3d179e700be
abdullahmorrison <abdullahmorrison@gmail.com>
2023-02-25 11:18:42 -0500

    set up panel components

---
+++ src/pages/panel/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Panel from './components/panel'
+
+export default function App(){
+  return (
+    <div>
+      <Panel/>
+    </div>
+  )
+}
+
---
+++ src/pages/panel/components/Panel.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import styles from "./panel.module.css";
+
+export default function Panel() {
+  return (
+    <div className={styles.panel}>
+      <h1>Streamer Bingo</h1>
+    </div>
+  );
+}
