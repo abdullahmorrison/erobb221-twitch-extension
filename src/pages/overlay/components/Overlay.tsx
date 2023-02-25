e91254682bae8c7aa09a38469c752e152edf31b7
abdullahmorrison <abdullahmorrison@gmail.com>
2023-02-25 16:57:16 -0500

    base overlay format and styles

--- src/pages/overlay/components/Overlay.tsx
+++ src/pages/overlay/components/Overlay.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import styles from './overlay.module.css'
 
+import Board from './board/Board'
+
 export default function Overlay(){
   return (
     <div className={styles.overlay}>
-      <h1>Streamer Bingo</h1>
+      <main>
+        <h1>Streamer Bingo</h1>
+        <Board />
+      </main>
+
+      <button>
+        <span>+</span>
+      </button>
     </div>
   )
 }
