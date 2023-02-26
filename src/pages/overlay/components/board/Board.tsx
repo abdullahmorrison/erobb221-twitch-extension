e33090a6942fa66ef3552c0faa8a40c74f691d23
abdullahmorrison <abdullahmorrison@gmail.com>
2023-02-25 19:48:41 -0500

    set up board data importing to component

--- src/pages/overlay/components/board/Board.tsx
+++ src/pages/overlay/components/board/Board.tsx
@@ -1,14 +1,27 @@
 import React from "react";
 import styles from "./board.module.css";
 
+import BoardData from '../../../../assets/board.json'
+
+interface Board{
+  img: {
+    src: string
+    altText: string
+  }
+  title: string
+}
 export default function Board() {
+  const [board, setBoard] = React.useState<Board[]>(BoardData)
+
   return (
     <div className={styles.board}>
-      {
-        <div className={styles.boardTile}>
-          <img src="" alt="Image" />
-          <p>Example of Tile Item</p>
-        </div>
+      { board.map((item: Board, index: number) => {
+        return (
+          <div key={index} className={styles.boardTile}>
+            <img src={item.img.src} alt={item.img.altText} />
+            <p>{item.title}</p>
+          </div>
+        )})
       }
     </div>
   );
