294137daa37765252136acc99c16d326258c425e
abdullahmorrison <abdullahmorrison@gmail.com>
2023-02-28 08:03:52 -0500

    open and close extension functionality

--- src/pages/overlay/components/Overlay.tsx
+++ src/pages/overlay/components/Overlay.tsx
@@ -4,8 +4,10 @@ import styles from './overlay.module.css'
 import Board from './board/Board'
 
 export default function Overlay(){
+  const [isExtensionOpen, setIsExtensionOpen] = React.useState(false)
+
   return (
-    <div className={styles.overlay}>
+    <div className={`${styles.overlay} ${styles.openExtensionButton} ${isExtensionOpen ? styles.open: styles.closed}`}>
       <main>
         <div className={styles.title}>
           <h1>Streamer Bingo </h1>
@@ -15,7 +17,7 @@ export default function Overlay(){
         <button className={styles.newGameButton}>New Game</button>
       </main>
 
-      <button className={styles.openExtensionButton}></button>
+      <button className={styles.openExtensionButton} onClick={() => setIsExtensionOpen(!isExtensionOpen)}></button>
     </div>
   )
 }
