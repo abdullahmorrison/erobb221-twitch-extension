15531b3bc40f0edd3d24a8481d09196b5f095ca2
abdullahmorrison <abdullahmorrison@gmail.com>
2023-03-15 21:04:25 -0400

    panel component set up

--- src/pages/panel/components/Panel.tsx
+++ src/pages/panel/components/Panel.tsx
@@ -1,11 +1,24 @@
 import React from "react";
 
+import Board from "../../../global/board/Board";
+import Instructions from "../../../global/instructions/Instructions";
+import Streamer from "../../../assets/streamer.json";
 import styles from "./panel.module.css";
 
 export default function Panel() {
+  const [streamer] = React.useState(Streamer)
+  const [isInstructionsOpen, setIsInstructionsOpen] = React.useState(false)
+
   return (
     <div className={styles.panel}>
-      <h1>Streamer Bingo</h1>
+      <title>
+          <h1>{streamer.name} Bingo </h1>
+          <button className={styles.help} onClick={()=>setIsInstructionsOpen(true)}>?</button>
+      </title>
+
+      {isInstructionsOpen ? <Instructions closeInstructions={()=>setIsInstructionsOpen(false)} /> : null}
+
+      <Board/>
     </div>
   );
 }
