c80c9598541ca1c53b3a04a082e64e77091b9b82
abdullahmorrison <abdullahmorrison@gmail.com>
2023-03-15 21:04:14 -0400

    panel app set up

--- src/pages/panel/App.tsx
+++ src/pages/panel/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Panel from './components/panel'
 
+import styles from './app.module.css'
+
 export default function App(){
   return (
-    <div>
+    <div className={styles.app}>
       <Panel/>
     </div>
   )
