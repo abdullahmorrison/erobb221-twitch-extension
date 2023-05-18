3732dac0ac33493e62438534167bf4add0a851f7
abdullahmorrison <abdullahmorrison@gmail.com>
2023-05-18 16:59:42 -0400

    tomato throwing

--- src/pages/overlay/App.tsx
+++ src/pages/overlay/App.tsx
@@ -1,12 +1,15 @@
 import React, { useState, useEffect, useCallback } from 'react'
 import BingoGame from './components/BingoGame/BingoGame'
+import Tomato from './components/Tomato/Tomato'
+import { Tomato as TomatoType } from './components/Tomato/types'
 import useChatCommand from './chatCommand'
 import { commands } from './commands'
 
 import styles from './app.module.css'
 
+
 export default function App(){
-  const [isBingoGameVisible, setIsBingoGameVisible] = React.useState(false)
+  const [isBingoTabVisible, setIsBingoTabVisible] = React.useState(false)
   const [isBingoGameOpen, setIsBingoGameOpen] = React.useState(false)
   const [isCursorVisible, setIsCursorVisible] = useState(true) // hiding the cursor when the mouse is idle on the screen
 
@@ -17,30 +20,58 @@ export default function App(){
     if (command === commands.showBingoGame) {
       showBingoGame(2)
       setCommand(commands.null)
+    }else if(command === commands.throwTomato) {
+      throwTomato()
+      setCommand(commands.null)
     }
   }, [command])
 
   const showBingoGame = useCallback((seconds: number) => {
     setIsCursorVisible(true)
-    setIsBingoGameVisible(true)
+    setIsBingoTabVisible(true)
     if(sleepTimer.current) clearTimeout(sleepTimer.current)
 
-    sleepTimer.current = setTimeout(() => {
+    sleepTimer.current = setTimeout(() => { // hide the cursor and the bingo game after set seconds
       setIsCursorVisible(false)
-      setIsBingoGameVisible(false)
+      setIsBingoTabVisible(false)
       setIsBingoGameOpen(false)
     }, seconds*1000)
+  }, [])
+
+
+  const [tomatoes, setTomatoes] = useState<TomatoType[]>([])
+  const throwTomato = useCallback(() => {
+    const newTomato: TomatoType = {
+      x: Math.random()*100,
+      y: Math.random()*100,
+      rotate: Math.random()*360,
+      splatter: false
+    }
+
+    setTomatoes(prev => [...prev, newTomato])
+    setTimeout(() => {
+      setTomatoes(tomatoes => tomatoes.map(tomato => {
+        if(tomato === newTomato) {
+          return {
+            ...tomato,
+            splatter: true
+          }
+        }
+        return tomato
+      }))
+    }, 500)
   }, [])
 
   return (
     <div
       className={`${styles.app} ${isCursorVisible? undefined : styles.cursorHidden}`}
       onMouseMove={()=>showBingoGame(5)}
-      onMouseLeave={()=>setIsBingoGameVisible(false)}
+      onMouseLeave={()=>setIsBingoTabVisible(false)}
       onClick={(event)=>isBingoGameOpen && event.target == event.currentTarget? setIsBingoGameOpen(false) : null}
     >
+      <Tomato tomatoes={tomatoes}/>
       <BingoGame
-        isBingoTabVisible={isBingoGameVisible}
+        isBingoTabVisible={isBingoTabVisible}
         isBingoGameOpen={isBingoGameOpen}
         setIsBingoGameOpen={setIsBingoGameOpen}
       />
--- src/pages/overlay/chatCommand.ts
+++ src/pages/overlay/chatCommand.ts
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import tmi, { ChatUserstate } from 'tmi.js'
 import Streamer from '../../assets/streamer.json'
+import { commands } from './commands'
 
 export default function chatCommand(){
   const [command, setCommand] = React.useState('')
@@ -15,14 +16,37 @@ export default function chatCommand(){
     ]
   }))
 
+
+  const tomatoTimer = React.useRef<NodeJS.Timeout | undefined>(undefined)
+  useEffect(() => {
+    return () => {
+      if(tomatoTimer.current) clearTimeout(tomatoTimer.current)
+    }
+  }, [])
+
    const messageHandler = React.useCallback((channel: string, tags: ChatUserstate, msg: string, self: boolean) => {
+    //if tomatoTimer is running and the msg contains the word "TomatoTime"
+    if(tomatoTimer.current && msg.includes('TomatoTime')) {
+      setCommand(commands.throwTomato)
+      return
+    }
+
     //ignore if user is not a moderator or broadcaster or if the user is not AbdullahMorrison
     if (!tags.mod && !tags.badges?.broadcaster && tags.username !== 'abdullahmorrison') return
     // Ignore echoed messages (messages sent by the bot) and messages that don't start with '!'
     if (self || !msg.trim().startsWith('!')) return
 
     const command = msg.trim().toLowerCase().slice(1)
-    if(command == 'bingo') setCommand(command)
+    if(command === commands.showBingoGame) setCommand(command)
+    else if(command === commands.throwTomato){
+      console.log('throwing tomatoes')
+      if(tomatoTimer.current) clearTimeout(tomatoTimer.current)
+      tomatoTimer.current = setTimeout(() => setCommand(''), 15*1000)
+    }else if(command === commands.cancel){
+      console.log('canceling tomatoes')
+      if(tomatoTimer.current) clearTimeout(tomatoTimer.current)
+      setCommand('')
+    }
   }, [])
 
   const connectedHandler = React.useCallback(() => {
